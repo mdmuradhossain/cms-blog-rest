3cb44eea7d678d7ec7a7385089b9e3bdacf52db1
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-08-07 22:11:37 +0600

    model,controller,repository,service,dto,security,config,mapper packages added

---
+++ src/main/java/io/murad/blog/rest/CmsBlogRestApplication.java
@@ -0,0 +1,13 @@
+package io.murad.blog.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsBlogRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsBlogRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/murad/blog/rest/CmsBlogRestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.murad.blog.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsBlogRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
