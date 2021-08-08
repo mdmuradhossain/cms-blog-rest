633a1cca62d9a4f903be86dbfcb9dd43bb2b5663
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-08-08 21:29:10 +0600

    VerificationTokenRepository added

---
+++ src/main/java/io/murad/blog/rest/repository/VerificationTokenRepository.java
@@ -0,0 +1,10 @@
+package io.murad.blog.rest.repository;
+
+import io.murad.blog.rest.model.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    Optional<VerificationToken> findByToken(String token);
+}
--- src/main/java/io/murad/blog/rest/service/AuthService.java
+++ src/main/java/io/murad/blog/rest/service/AuthService.java
@@ -2,6 +2,7 @@ package io.murad.blog.rest.service;
 
 import io.murad.blog.rest.dto.RegisterRequest;
 import io.murad.blog.rest.model.User;
+import io.murad.blog.rest.model.VerificationToken;
 import io.murad.blog.rest.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -16,6 +18,7 @@ public class AuthService {
 
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
 
     @Transactional
     public void register(RegisterRequest registerRequest) {
@@ -27,5 +30,15 @@ public class AuthService {
         user.setEnabled(false);
 
         userRepository.save(user);
+
+        String token = generateVerificationToken(user);
+    }
+
+    private String generateVerificationToken(User user){
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+        verificationTokenRepository
     }
 }
