81b6b0f642b9dc5541765e9d478a835af24c0044
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-08-08 20:52:47 +0600

    AuthController added

---
+++ src/main/java/io/murad/blog/rest/controller/AuthController.java
@@ -0,0 +1,18 @@
+package io.murad.blog.rest.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    @PostMapping("/signup")
+    public void registerUser(@RequestBody RegisterRequest registerRequest){
+
+    }
+}
---
+++ src/main/java/io/murad/blog/rest/dto/RegisterRequest.java
@@ -0,0 +1,11 @@
+package io.murad.blog.rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+}
