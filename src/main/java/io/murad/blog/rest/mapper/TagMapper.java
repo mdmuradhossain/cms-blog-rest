72fee89c2905189ff33694718a56a4df01a82931
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-08-10 21:47:39 +0600

    TagMapper added

--- src/main/java/io/murad/blog/rest/mapper/TagMapper.java
+++ src/main/java/io/murad/blog/rest/mapper/TagMapper.java
@@ -1,9 +1,24 @@
 package io.murad.blog.rest.mapper;
 
+import io.murad.blog.rest.dto.TagDto;
+import io.murad.blog.rest.model.Tag;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface TagMapper {
 
+    @Mappings({
+            @Mapping(target = "id",source = "tag.tagId"),
+            @Mapping(target = "name",source = "tag.tagName")
+    })
+    TagDto mapToTagDto(Tag tag);
+
+    @Mappings({
+            @Mapping(target = "tagId", source = "tagDto.id"),
+            @Mapping(target = "tagName", source = "tagDto.name"),
+    })
+    Tag mapToTag(TagDto tagDto);
 
 }
