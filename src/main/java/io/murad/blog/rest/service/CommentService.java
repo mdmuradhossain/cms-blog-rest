4f893249da9223877a57f39fd27bfffeb35d5ac2
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-08-13 21:29:08 +0600

    CommentService added

---
+++ src/main/java/io/murad/blog/rest/service/CommentService.java
@@ -0,0 +1,43 @@
+package io.murad.blog.rest.service;
+
+import io.murad.blog.rest.dto.CommentDto;
+import io.murad.blog.rest.exception.PostNotFoundException;
+import io.murad.blog.rest.mapper.CommentMapper;
+import io.murad.blog.rest.model.Comment;
+import io.murad.blog.rest.model.NotificationEmail;
+import io.murad.blog.rest.model.Post;
+import io.murad.blog.rest.model.User;
+import io.murad.blog.rest.repository.CommentRepository;
+import io.murad.blog.rest.repository.PostRepository;
+import io.murad.blog.rest.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CommentService {
+
+    private static final String POST_URL = " ";
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final CommentRepository commentRepository;
+    private final AuthService authService;
+    private final MailService mailService;
+    private final CommentMapper commentMapper;
+    private final MailContentBuilder mailContentBuilder;
+
+    public void saveComment(CommentDto commentDto) {
+        Post post = postRepository.findById(commentDto.getPostId()).orElseThrow(() -> new PostNotFoundException((commentDto.getPostId().toString()));
+        User user = authService.getCurrentUser();
+        Comment comment = commentMapper.mapToComment(commentDto, post, user);
+        commentRepository.save(comment);
+
+        //Send message for comment
+        String message = mailContentBuilder.build(post.getUser().getUserName() + " posted a comment on your post." + POST_URL);
+        sendCommentNotification(message, post.getUser());
+    }
+
+    private void sendCommentNotification(String message, User user) {
+        mailService.sendMail(new NotificationEmail(user.getUserName() + " Commented on your post", user.getEmail(), message));
+    }
+}
