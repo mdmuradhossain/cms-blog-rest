d656c21d53759becc92a0b9af5e7b5b56c220da6
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-08-14 20:14:49 +0600

    refreshTokens method and logout added into AuthController

--- src/main/java/io/murad/blog/rest/controller/AuthController.java
+++ src/main/java/io/murad/blog/rest/controller/AuthController.java
@@ -2,12 +2,19 @@ package io.murad.blog.rest.controller;
 
 import io.murad.blog.rest.dto.AuthenticationRequest;
 import io.murad.blog.rest.dto.AuthenticationResponse;
+import io.murad.blog.rest.dto.RefreshTokenRequest;
 import io.murad.blog.rest.dto.RegisterRequest;
 import io.murad.blog.rest.service.AuthService;
+import io.murad.blog.rest.service.RefreshTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
 
 @RestController
 @RequestMapping("/auth")
@@ -15,22 +22,34 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/signup")
-    public ResponseEntity<String> registerUser(@RequestBody RegisterRequest registerRequest){
+    public ResponseEntity<String> registerUser(@RequestBody RegisterRequest registerRequest) {
         authService.register(registerRequest);
         return new ResponseEntity<>("User Registration Successful", HttpStatus.OK);
     }
 
     @GetMapping("/accountVerification/{token}")
-    public ResponseEntity<String> verifyAccount(@PathVariable String token){
+    public ResponseEntity<String> verifyAccount(@PathVariable String token) {
         authService.verifyAccount(token);
-        return new ResponseEntity<>("Account Activated Successfully",HttpStatus.OK);
+        return new ResponseEntity<>("Account Activated Successfully", HttpStatus.OK);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> signIn(@RequestBody AuthenticationRequest authenticationRequest){
-    AuthenticationResponse authenticationResponse = authService.signIn(authenticationRequest);
-    return new ResponseEntity<>(authenticationResponse,HttpStatus.OK);
+    public ResponseEntity<AuthenticationResponse> signIn(@RequestBody AuthenticationRequest authenticationRequest) {
+        AuthenticationResponse authenticationResponse = authService.signIn(authenticationRequest);
+        return new ResponseEntity<>(authenticationResponse, HttpStatus.OK);
+    }
+
+    @PostMapping("/refresh/token")
+    public AuthenticationResponse refreshTokens(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException {
+        return authService.refreshToken(refreshTokenRequest);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> signOut(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        refreshTokenService.deleteRefreshToken(refreshTokenRequest.getRefreshToken());
+        return new ResponseEntity<>("Refresh token deleted successfully", HttpStatus.OK);
     }
 }
