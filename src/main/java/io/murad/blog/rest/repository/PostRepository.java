cb912df5f271911e4a4bd770daa24201aa0e0aeb
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-08-19 21:53:21 +0600

    findAllByUser method added into PostRepo

--- src/main/java/io/murad/blog/rest/repository/PostRepository.java
+++ src/main/java/io/murad/blog/rest/repository/PostRepository.java
@@ -1,8 +1,12 @@
 package io.murad.blog.rest.repository;
 
 import io.murad.blog.rest.model.Post;
+import io.murad.blog.rest.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post,Long> {
 
+    List<Post> findAllByUser(User user);
 }
