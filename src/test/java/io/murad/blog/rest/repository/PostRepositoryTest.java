8696cae3cae759a92afbc6df4f60cfac045e95d2
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-12-12 21:35:58 +0600

    PostRepositoryTest added

---
+++ src/test/java/io/murad/blog/rest/repository/PostRepositoryTest.java
@@ -0,0 +1,22 @@
+package io.murad.blog.rest.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class PostRepositoryTest {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @BeforeEach
+    void setUp() {
+    }
+}
